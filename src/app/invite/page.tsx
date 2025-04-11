6184ce07a2608279bf3f729c6338c0bc54f2e037
Clemens Stefan Heithecker <clemens@heithecker.dev>
2025-04-12 01:03:16 +0200

    feat: update colors

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -7,8 +7,8 @@ const ORGANIZATION_NAME = "Reitsportkonzepte Völker";
 
 export default function InvitePage() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+    <div className="flex min-h-screen items-start justify-items-center gap-16 px-4 py-8 font-[family-name:var(--font-geist-sans)] sm:items-center sm:p-20">
+      <main className="bg-background row-start-2 flex flex-col items-center gap-[32px] rounded-2xl px-4 py-6 sm:items-start sm:p-6">
         <Heading>Dein Reitabzeichentraining</Heading>
         <Text>
           Freu dich auf ein einzigartiges Trainingserlebnis bei{" "}
